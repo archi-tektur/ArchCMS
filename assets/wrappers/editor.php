ab4565062a0d67a9a29e2922b57a2015151bf0fa
Oskar Barcz <kontakt@archi-tektur.pl>
2018-11-27 23:36:40 +0100

    Created simple SSR page template

--- assets/wrappers/editor.php
+++ assets/wrappers/editor.php
@@ -1,10 +1,3 @@
 <?php
 
-use ArchAPI\Controller\CMS;
-
-if(isset($_GET['id']))
-    $data = CMS::getEditorPost($_GET['id']);
-else
-    return ["data" => "Data read problem"];
-
-return ['data' => $data];
+return [];
